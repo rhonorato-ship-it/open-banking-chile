8b4d051d153ac937d0f0d07dca896de22d8a2cac
rhonorato-ship-it <rhonorato@trytoku.com>
2026-03-25 20:42:45 -0300

    fix: update postgres client configuration to set max connections

--- web/lib/db.ts
+++ web/lib/db.ts
@@ -2,5 +2,5 @@ import postgres from "postgres";
 import { drizzle } from "drizzle-orm/postgres-js";
 import * as schema from "./schema";
 
-const client = postgres(process.env.DATABASE_URL!, { prepare: false });
+const client = postgres(process.env.DATABASE_URL!, { max: 1 });
 export const db = drizzle(client, { schema });
